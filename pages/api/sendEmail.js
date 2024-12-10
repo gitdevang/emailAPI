0a675eb9d4e7477414a9a0d37a60d2966652a27e
Qualipact <qualipact@gmail.com>
2024-12-10 19:07:29 +0530

    Initial commit

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -4,33 +4,36 @@ import nodemailer from "nodemailer";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { name, mobile, email, services, message } = req.body;
-    let company;
-    if (req.body.company) {
-      company = req.body.company;
+
+    // Make sure required fields are present
+    if (!name || !mobile || !email || !services || !message) {
+      return res.status(400).json({ error: "Missing required fields!" });
     }
 
-    // Nodemailer setup (yahan tum apne email configuration daaloge)
+    let company = req.body.company && req.body.company.length > 0 ? req.body.company : '';
+
+    // Nodemailer setup
     const transporter = nodemailer.createTransport({
-      service: "gmail", // Use your email provider
+      service: "gmail", // Gmail
       auth: {
         user: process.env.EMAIL_USER, // Your email address
         pass: process.env.EMAIL_PASS, // Your email password
       },
     });
 
-    // Email options (who will receive the email)
+    // Email options
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: process.env.TO_EMAIL, // Email where you want to receive the form data
       subject: `Business Enquiry from ${name}`,
       text: `
-            Name: ${name}\n
-            Email: ${email}\n
-            Mobile: ${mobile}\n
-            Services: ${services}\n
-            ${company ? `Company : ${company}\n` : null}
-            Message: ${message}
-          `,
+        Name: ${name}\n
+        Email: ${email}\n
+        Mobile: ${mobile}\n
+        Services: ${services}\n
+        ${company.length > 0 ? `Company: ${company}\n` : ''}
+        Message: ${message}
+      `,
     };
 
     try {
@@ -38,7 +41,8 @@ export default async function handler(req, res) {
       await transporter.sendMail(mailOptions);
       res.status(200).json({ message: "Your message sent successfully!" });
     } catch (error) {
-      res.status(500).json({ error: "Error Occured - Something went wrong" });
+      console.error("Error details:", error.message); // Log the error message
+      res.status(500).json({ error: "Error Occurred - Something went wrong" });
     }
   } else {
     res.status(405).json({ error: "Error: Method Not Allowed" });
